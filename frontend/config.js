2242bf60102435d57619ebb38c012a3f438d9f6c
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-08-26 10:47:15 -0500

    front

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 // Configuración global para obtener la URL del backend desde Vite
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+export const BACKEND_URL = "https://caso-practico-final-1.onrender.com";
